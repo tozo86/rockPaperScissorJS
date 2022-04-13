21d6022b5ce2c0d2d3fcdb7bb91cfb64dbd8cf4b
tozo86 <tozo86@gmail.com>
2022-04-13 12:50:11 +0200

    Made UI for the game

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,8 +1,7 @@
 //5 rounded rock-paper-Scissor game. My first JS app...
 //to play type in Console: game();
 
-let playerScore;
-let computerScore;
+
 
 // computer's choice
 function computerPlay() {
@@ -24,22 +23,25 @@ function computerPlay() {
 }
 
 // playing 1 round
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     const player = playerSelection.toLowerCase();
-
+    const computerSelection = computerPlay();
     if (player === computerSelection) {
-        return "Draw";
+        return 'Draw';
     }
     else if ((player === "rock" && computerSelection === "paper") || (player === "paper" && computerSelection === "scissors") || (player === "scissors" && computerSelection === "rock")) {
         computerScore++;
-        return `You Lose! ${computerSelection} beats ${player}`;
+        return `You Lose! ${computerSelection.toUpperCase()} beats ${player.toUpperCase()}!`;
     }
     else {
         playerScore++;
-        return `You Win! ${player} beats ${computerSelection}`;
+        return `You Win! ${player.toUpperCase()} beats ${computerSelection.toUpperCase()}!`;
     }
+    console.log(this);
 }
-// request player selection
+// request player selection 
+// OLD STUFF... can be deleted
+/*
 function playerChoice() {
     let choice = "";
     while (true) {
@@ -51,25 +53,75 @@ function playerChoice() {
             choice = prompt("Please choose stone, paper or scissors?");
         }
     }
-}
+} */
 
 //game playing 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log(`player score: ${playerScore} computer score: ${computerScore}`);
-        console.log(playRound(playerChoice(), computerPlay()));
-        
-    }
+function whoWin() {
 
-    if (computerScore===playerScore) {
+    if (computerScore === playerScore) {
         return `${playerScore} : ${computerScore} - Draw. Nobody win.`;
     }
-    else if (computerScore>playerScore) {
+    else if (computerScore > playerScore) {
         return `${playerScore} : ${computerScore} - Computer Win!`;
     }
     else {
         return `${playerScore} : ${computerScore} - You Win!`;
     }
 }
+//finished 5 round
+function finished() {
+
+
+
+}
+
+//new game
+function newGame() {
+    sumRound = 0;
+    playerScore = 0;
+    computerScore = 0;
+    p1.style.display = 'none';
+    btn_newGame.style.display = 'none';
+}
+let playerScore = 0;
+let computerScore = 0;
+
+let container = document.querySelector('.container');
+let p_score = document.querySelector('.scores');  //p
+p_score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+
+let p_roundResult = document.querySelector('.roundResult');
+p_roundResult.style.visibility = 'hidden';
+
+let p1 = document.createElement('p');
+container.appendChild(p1);
+
+let btn_newGame = document.createElement('button');
+btn_newGame.textContent = 'New Game';
+container.appendChild(btn_newGame);
+btn_newGame.addEventListener('click', () => newGame());
+
+
+newGame();
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        p_roundResult.textContent = playRound(button.id);
+        p_score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+        p_roundResult.style.visibility = 'visible';
+        sumRound++;
+        if (sumRound === 5) {
+            p1.textContent = whoWin();
+            p1.style.display = 'block';
+            btn_newGame.setAttribute('style', 'display: inline-block; width:200px; background-image: linear-gradient(-180deg, #e23737 0%, #c81e1e 100%);');
+
+        }
+    });
+});
+
+
+/*let btn_rock = document.querySelector('#rock');
+btn_rock.addEventListener('click', () => playRound(btn_rock.id));
+
+console.log(btn_rock);
+*/
